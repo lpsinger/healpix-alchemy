6c5ac81614ce1fcc6b275752b1f527ebbfca412b
Leo Singer <leo.singer@ligo.org>
2020-08-11 13:19:46 -0400

    Do not use SQL trigonometric functions
    Not all SQL servers support `cos` and `sin`. SQLite, for example,
    lacks these functions.
    
    Evaluating theses special functions on the Python side is a pattern
    that we will need to use for the HEALPix forward and reverse
    transformations.

--- healpix_alchemy/point.py
+++ healpix_alchemy/point.py
@@ -1,14 +1,41 @@
 """Spatial indexing for astronomical point coordinates."""
+import numpy as np
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
 from sqlalchemy.schema import Column, Index
-from sqlalchemy.sql import and_
+from sqlalchemy.sql import and_, between
 from sqlalchemy.types import Float
 
-from .math import sind, cosd
 from .util import InheritTableArgs
 
 __all__ = ('Point',)
+
+
+def get_x_for_ra_dec(ra, dec):
+    return np.cos(np.deg2rad(ra)) * np.cos(np.deg2rad(dec))
+
+
+def get_y_for_ra_dec(ra, dec):
+    return np.sin(np.deg2rad(ra)) * np.cos(np.deg2rad(dec))
+
+
+def get_z_for_ra_dec(ra, dec):
+    return np.sin(np.deg2rad(dec))
+
+
+def get_x_for_context(context):
+    params = context.get_current_parameters()
+    return get_x_for_ra_dec(params['ra'], params['dec'])
+
+
+def get_y_for_context(context):
+    params = context.get_current_parameters()
+    return get_y_for_ra_dec(params['ra'], params['dec'])
+
+
+def get_z_for_context(context):
+    params = context.get_current_parameters()
+    return get_z_for_ra_dec(params['ra'], params['dec'])
 
 
 class Point(InheritTableArgs):
@@ -18,9 +45,15 @@ class Point(InheritTableArgs):
         super().__init__(*args, **kwargs)
         self.ra = ra
         self.dec = dec
+        self.x = get_x_for_ra_dec(ra, dec)
+        self.y = get_y_for_ra_dec(ra, dec)
+        self.z = get_z_for_ra_dec(ra, dec)
 
     ra = Column(Float)
     dec = Column(Float)
+    x = Column(Float, default=get_x_for_context, onupdate=get_x_for_context)
+    y = Column(Float, default=get_y_for_context, onupdate=get_y_for_context)
+    z = Column(Float, default=get_z_for_context, onupdate=get_z_for_context)
 
     @hybrid_property
     def cartesian(self):
@@ -32,9 +65,7 @@ class Point(InheritTableArgs):
             A tuple of the x, y, and z coordinates.
 
         """
-        return (cosd(self.ra) * cosd(self.dec),
-                sind(self.ra) * cosd(self.dec),
-                sind(self.dec))
+        return (self.x, self.y, self.z)
 
     @hybrid_method
     def within(self, other, radius):
@@ -52,10 +83,10 @@ class Point(InheritTableArgs):
         bool
 
         """
-        sin_radius = sind(radius)
-        cos_radius = cosd(radius)
+        sin_radius = np.sin(np.deg2rad(radius))
+        cos_radius = np.cos(np.deg2rad(radius))
         carts = (obj.cartesian for obj in (self, other))
-        terms = ((lhs.between(rhs - 2 * sin_radius, rhs + 2 * sin_radius),
+        terms = ((between(lhs, rhs - 2 * sin_radius, rhs + 2 * sin_radius),
                   lhs * rhs) for lhs, rhs in zip(*carts))
         bounding_box_terms, dot_product_terms = zip(*terms)
         return and_(*bounding_box_terms, sum(dot_product_terms) >= cos_radius)
